49744f4a591b80204667aa368c7af2328b0b1a76
Bharat Chatla <bharatkc@3c07543f4f44.ant.amazon.com>
2015-05-16 09:36:53 -0700

    initial commit

---
+++ src/main/java/com/uber/email/UberEmailApplication.java
@@ -0,0 +1,12 @@
+package com.uber.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberEmailApplication.class, args);
+    }
+}
